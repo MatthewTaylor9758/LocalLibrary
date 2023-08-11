2df703b8912a711f95fac500fab33410c0129459
Matthew Taylor <matthewtaylor9758@gmail.com>
2023-08-11 14:38:59 -0400

    add class based views

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
-
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 ]
+
+# For Django class-based views we access an appropriate view function by calling the
+# class method as_view(). This does all the work of creating an instance of the class,
+# and making sure that the right handler methods are called for incoming HTTP requests.
