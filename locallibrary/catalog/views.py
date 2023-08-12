82a575468a13503e2a876943b7c029ef978cee96
Matthew Taylor <matthewtaylor9758@gmail.com>
2023-08-12 11:58:45 -0400

    add auth path to urls.py(top level)

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -16,6 +16,9 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     num_fantasy_books = Book.objects.filter(genre__name__icontains='fantasy').count()
 
     context = {
@@ -23,6 +26,7 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
         'num_fantasy_books': num_fantasy_books,
     }
 
